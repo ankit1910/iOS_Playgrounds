444f452ae6fb0611fcc93d813ba2f9d9b593ee29
ankit1910 <ankit.bansal@vinsol.com>
2016-03-14 13:38:32 +0530

    update content.swift

--- Interest.playground/Contents.swift
+++ Interest.playground/Contents.swift
@@ -3,14 +3,34 @@
 import UIKit
 
 class Interest {
-    let static rate = 10
-    let principle :Double
-    let time: Int
+    static let rate = 10.0
+    let principal :Double
+    let time: Double
     
-    init(principle: Double, time: Int) {
-        self.principle = principle
+    init(principal: Double, time: Double) {
+        self.principal = principal
         self.time = time
     }
     
+    func simpleInterest() -> Double {
+        return(principal * (time) * (Interest.rate/100))
+    }
     
+    func compoundInterest() -> Double {
+        return((principal * pow((1+(Interest.rate/100)), time)) - principal)
+    }
+    
+    func differenceInterest() -> Double {
+        return(compoundInterest() - simpleInterest())
+    }
 }
+
+
+var i = Interest(principal: 1000.0, time: 2.0)
+
+i.simpleInterest()
+i.compoundInterest()
+i.differenceInterest()
+
+
+
